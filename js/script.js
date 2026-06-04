9cf0a8be354c40d9f1591066e9459420d2f4b64b
onyangoedwin <onyango.edwin@strathmore.edu>
2026-06-04 12:05:22 +0300

    Lab 2.7

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let age=20;
+var course="BBIT";
+const university="Strathmore";
+
+//Redeclareing a variable
+let age=22;
+var course="BCOM";
+const university="Riara";
+
+if(true){
+    let time = "11:30";
+    var day = "Monday";
+    const year =2024
+}
+console.log(day);
