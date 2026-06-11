59b06522d7d08147b51fda44833e71c52f9cf42c
onyangoedwin <onyango.edwin@strathmore.edu>
2026-06-11 12:03:31 +0300

    lab 2.9 js arrays and objects

--- js/script.js
+++ js/script.js
@@ -52,3 +52,107 @@ console.log(add(4, 2))
 const multiply=(x, y)=>x  * y;
 console.log(multiply(2, 5));
 
+/**Javascript Arrays (related) */
+
+let courseScores=[80, 90, 75, 85];
+
+//access array name[index]
+//80
+console.log(courseScores[4]);
+
+//90
+console.log(scores[2]);
+
+//75
+console.log(scores[0]);
+
+
+//85
+console.log(scores[1]);
+let names=["Omondi", "Wafula", "Kiprotich", "Nyambane", " Edwin"];
+
+//print out Kiprotich on the console window
+console.log(student_names[2]);
+
+let governors=[
+    [47,"Johnson Sakaja"],
+    [1,"AbdulSwamad Sharif"],
+    [21,"Irungu Kang'ata"],
+];
+//The governor of county number 1 is AbdulSwamad Sharif
+console.log("The governor of county number "+governors[1][0]+" is "+governors[4][1]);
+
+
+//map(perform an operation on each element)
+let doubledScores = courseScores.map(x => x * 2);
+console.log(doubledScores);
+
+//properties
+//The class has j students(student_names)
+console.log("The class has "+student_names.length+" students");
+
+//Kenya has b governors
+console.log("Kenya has "+governors.length+" governors");
+
+//for in
+for(let index in scores){
+    console.log(scores[index]);
+}
+//for of
+for(let score of scores){
+    console.log(score);
+}
+forEach
+scores.forEach(function(score){
+    console.log("score: "+score);
+});
+
+const student={
+    name:"Edwin",
+    age: 20,
+    course: "BBIT", 
+    passed: true
+    grade:'A',
+    "admission number": 12345,
+    group:"2A"
+    attendance: 20,
+    addAttendance: function(){
+        this.attendance=this.attendance+1;
+
+    }
+};
+
+//accessing items
+//student name
+console.log("My name is",
+   student.name,"from",
+   student["admission number"],
+   "university"
+);
+
+//add attendance
+student.addAttendance(); //add attendance by 1
+console.log(student.attendance);//21
+
+//array of objects
+let bbit_2b_students=[
+    {adm:22238, name : "Blessing"},
+    {adm:22239, name : "Ryan"},
+    {adm:22240, name : "Ambrose"},
+    {adm:22241, name : "Wafula"}
+
+//print out the name of all students in bbit_2b_students using a loop
+bbit_2b_students.forEach(function(student){
+    console.log(student);
+});
+//getting the keys of an object as an array
+console.log(student.keys().toArray());
+
+//getting the values of an object as an array
+console.log(student.values().toArray());
+
+//getting both keys and values of an object as an array
+console.log(student.entries().toArray());
+
+
+
